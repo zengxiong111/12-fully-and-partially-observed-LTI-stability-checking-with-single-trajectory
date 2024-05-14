e4af4fa7e8a7508705c5e9179034a094c2474bc1
Xiong Zeng <zengxion@umich.edu>
2024-05-14 17:10:47 -0400

    1

--- stability_checking.m
+++ stability_checking.m
@@ -9,7 +9,7 @@ n = 2;      %n is the state dimension
 m = 1;      %m is the output dimension
 p = 1;      %p is the input dimension
 
-N=70;
+N=10000;
 T=3*n;
 
 % for i = 1:5
@@ -28,12 +28,18 @@ T=3*n;
 %counterexample: Jordan block
 %A = [1.003 0;0 0.9];
 %A = [1.001 0;0 0.9];
-A = [0.9 1;0 0.99];
-eig(A'*A-eye(n))
+%A = [0.99 1;0 0.99];
+%A = [0.99 0;0 0.99];
+%A = [0.99 1;0 0.99];
+%A = [0.99 1 0;0 0.99 1;0 0 0.99];
+%A = [1.01 1 0;0 0.99 1;0 0 0.99];
+norm(A)
+n=size(A,1);
+eig(A'*A-eye(n));
 t=3;
 temp = (A')^t*(A'*A-eye(n))*A^t;
 % svd(temp)
- eig(temp)
+ eig(temp);
 % trace(temp)
 % ones(n,1)'*temp*ones(n,1)
 %A = [1 -1;1 1];
@@ -44,15 +50,19 @@ temp = (A')^t*(A'*A-eye(n))*A^t;
 %A = [0.9 0.9;0 0.5];
 %A = [0.9 0;0 0.5];
 B = zeros(n,p);
-C = zeros(m,n);
+C = ones(m,n);
+C(1)=1;
 D = zeros(m,p);
 
 Q = eye(n);
 R = eye(p);
 
 sigma_u_2 = 0;
-sigma_w_2 = 0.00;
-sigma_v_2 = 0.00;
+% sigma_w_2 = 0.001;
+% sigma_v_2 = 0.001;
+
+sigma_w_2 = 0.001;
+sigma_v_2 = 0.001;
 
 x_0 = ones(n,1);
 
@@ -80,12 +90,12 @@ end
 % figure;
 % hold on;
 %plot(time_index,X_norm,time_index,X_diff_norm,'-x','LineWidth',3); % plot the tight bound eq (4) in the paper
-plot(time_index,X_norm,'-x','LineWidth',3); % plot the tight bound eq (4) in the paper
+plot(time_index,X_norm,time_index(1:end-1),Y_norm(1:end-1),'-x','LineWidth',3); % plot the tight bound eq (4) in the paper
 %plot(time_index,X_diff_norm,'-x','LineWidth',3); % plot the tight bound eq (4) in the paper
 
  %legend('robustness margin from Hinf','LMI sufficient [0,+m]','LMI sufficient [-m,+m]','controllability margin')
 %legend('||x(t)||_2','||x(t+1)||_2-||x(t)||_2')
-legend('||x(t)||_2')
+legend('||x_t||_2','||y_t||_2','Location','best')
 %legend('||x(t+1)||_2-||x(t)||_2')
 grid on;
 ax = gca;
@@ -96,5 +106,9 @@ lgd.FontSize = 18;
 xlabel('time t','FontSize',18) ;
 %ylabel('||x||_2','FontSize',18) ;
 set(gca,'FontSize',20)
+
+%Yingying's method
+
+%Ali's method
 
 
